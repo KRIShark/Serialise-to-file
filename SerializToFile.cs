7bd53e70fc4f315bd3c3bc401f4e8471b606d29e
KRIShark <cristofnagy@gmail.com>
2021-05-02 21:55:45 +0200

    Serialise to file

---
+++ SerializToFile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+class SerializToFile
+{
+	public string Serializ<T>(T obj,string fileName) 
+	{
+		IFormatter formatter = new BinaryFormatter();
+		Stream stream;
+
+		string message = "successful";
+		if (obj == null) return "Given object is null";
+		try
+		{
+			stream = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+			formatter.Serialize(stream, obj);
+			stream.Close();
+		}
+		catch (Exception ex)
+		{
+			message = ex.Message;
+		}
+
+		return message;
+	}
+
+	public T DeSerializ<T>(string fileName) 
+	{
+		IFormatter formatter = new BinaryFormatter();
+		Stream stream;
+		T objectOut = default(T);
+		try
+		{
+			stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+			objectOut = (T)formatter.Deserialize(stream);
+			stream.Close();
+		}
+		catch (Exception)
+		{
+			//Error
+		}
+		return objectOut;
+	}
+}
